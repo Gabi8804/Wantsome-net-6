3c3d5d1b45153d63cc776040a2cda5e9173dddb1
Gabi8804 <gabriel88popa@yahoo.com>
2020-07-20 20:01:19 +0300

    Tema1 + Tema 2

---
+++ week01/course/Week01Conversions/Program.cs
@@ -0,0 +1,371 @@
+﻿using System;
+
+namespace Week01HomeWork
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the exercise number you wish to run");
+            int Ex = int.Parse(Console.ReadLine());
+            SelectExercise(Ex);
+
+            
+            // 1. Display even numbers between 0, 100            
+
+
+
+
+            // 2.
+
+            /* * Read 2 number from the keyboard and create mathematical operations for
+                -division
+                -multiplication
+                -on division if the number is 0 display a message that it is not possible
+                -calculate the minimum and maximum
+             */
+
+
+
+            //3
+            /*
+             * Read a number from the keyboard and
+                -it is the temperature in C, convert it to F
+                -it is the temperature in F, convert it to C
+                -it is a number of days, convert it to years, months, weeks and days
+                Conventions - year: 365 days, month: 30 days, week: 7 days, use only numbers
+                bigger than 1000
+            */
+
+
+            //4. Print to console all even numbers between 15 and 97.            
+
+
+
+            //5. Print all numbers divisible by 3 between 20 and 65.            
+
+
+            //6. Count all numbers divisible by 7 and multiple of 5, from 1400 to 2300 and print the result to console.            
+
+            //7. Write a program to guess a number between 1 and 10. To generate a random number, use Random class from .NET Framework.            
+
+            //9. Read a text from console and print it reversed.            
+
+            //10. Print numbers from 1 to 10 except 4 and 7.            
+
+            //11. Print the Fibonacci sequence from 0 to 50. (i.e. Exery next number is found by adding up those two before it: 0, 1, 1, 2, 3, 5, 8, 13, ..
+
+        }
+        public static void SelectExercise(int val)
+        {
+            switch (val)
+            {
+                case 1:
+                    Ex01();
+                    break;
+                case 2:
+                    Ex02();
+                    break;
+                case 3:
+                    Ex03();
+                    break;
+                case 4:
+                    Ex04();
+                    break;
+                case 5:
+                    Ex05();
+                    break;
+                case 6:
+                    Ex06();
+                    break;
+                case 7:
+                    Ex07();
+                    break;
+                case 9:
+                    Ex09();
+                    break;
+                case 10:
+                    Ex10();
+                    break;
+                case 11:
+                    Ex11();
+                    break;
+                case 12:
+                    Ex12();
+                    break;
+                case 13:
+                    Ex13();
+                    break;
+                case 14:
+                    Ex14();
+                    break;
+                case 15:
+                    Ex15();
+                    break;
+                case 16:
+                    Ex16();
+                    break;
+
+            }
+        }
+
+        public static void Ex01()
+        {
+            for (int i = 0; i <= 100; i += 1)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("the number is even: " + i);
+                }
+
+            }
+        }
+
+        public static void Ex02()
+        {
+            Console.WriteLine("First number");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second number");
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number1 != 0 && number2 != 0)
+            {
+                float number3 = (float)number1 / (float)number2;
+                Console.WriteLine("Division= " + number3);
+            }
+
+            int numberMultiplied = number1 * number2;
+            Console.WriteLine("Multiplication= " + numberMultiplied);
+            if (number1 > number2)
+            {
+                Console.WriteLine("The greatest number is: " + number1 + " and the lowest number is " + number2);
+            }
+            else
+            {
+                Console.WriteLine("The greatest number is: " + number2 + " and the lowest number= " + number1);
+            }
+        }
+
+        public static void Ex03()
+        {
+            Console.WriteLine("Enter your number");
+            int tempNumber = int.Parse(Console.ReadLine());
+
+            float F = tempNumber * 1.8f + 32;
+            float C = (tempNumber - 32) / 1.8f;
+            Console.WriteLine("Fahrenheit temp is: " + F);
+            Console.WriteLine("Celsius temp is: " + C);
+
+            Console.WriteLine("Enter number of days");
+
+            int days = int.Parse(Console.ReadLine());
+            while (days < 1000)
+            {
+                Console.WriteLine("Enter a number bigger than 1000");
+                days = int.Parse(Console.ReadLine());
+
+
+            }
+
+            if (days > 1000)
+            {
+                int years = days / 365;
+                int months = days / 30;
+                int weeks = days / 7;
+
+                Console.WriteLine("You have entered " + years + " years, " + months + " months, " + weeks + " weeks.");
+            }
+
+
+
+        }
+        public static void Ex04()
+        {
+            for (int i = 15; i <= 97; i += 1)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("the number is even: " + i);
+                }
+
+            }
+        }
+
+        public static void Ex05()
+        {
+            for (int i = 20; i <= 65; i += 1)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("the number" + i + " is divisible by 3");
+                }
+
+            }
+        }
+
+        public static void Ex06()
+        {
+            int a = 0;
+            for (int i = 1400; i <= 2300; i++)
+            {
+                if (i % 7 == 0 && i % 5 == 0)
+                {
+                    a = a + 1;
+                }
+            }
+            Console.WriteLine("There are " + a + " numbers divisible by 7 and multiple of 5 between 1400 and 2300");
+        }
+
+        public static void Ex07()
+        {
+            int x = new Random().Next(1, 10);
+            Console.WriteLine("Guess the random number");
+            int y = Convert.ToInt32(Console.ReadLine());
+            while (y != x)
+            {
+                Console.WriteLine("Wrong guess, random number is " + x);
+                y = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+
+
+        }
+
+        public static void Ex09()
+        {
+            Console.WriteLine("Enter your string");
+            string y = Console.ReadLine();
+            string reverseY = "";
+
+            for (int i = y.Length - 1; i > -1; i--)
+            {
+                reverseY = reverseY + y[i];
+            }
+
+            Console.WriteLine("Your reverse string is: " + reverseY);
+        }
+
+        public static void Ex10()
+        {
+            Console.WriteLine("Numbers from 1 to 10 except 4 and 7 are");
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i != 4 && i != 7)
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+        }
+
+        public static void Ex11()
+        {
+            long a = 0;
+            long b = 1;
+            long c = 0;
+            int length = 50;
+            Console.WriteLine("Fibonacci sequence from 0 to 50");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 0; i <= length; i++)
+            {
+                 c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+        }
+        public static void Ex12()
+        {
+            Console.WriteLine("Input the first number");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the second number");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int sum = number1 + number2;
+            int div = number1 - number2;
+            int mult = number1 * number2;
+            int sub = number1 / number2;
+            int mod = number1 % number2;
+
+            Console.WriteLine(number1 + " + " + number2 + " = " + sum);
+            Console.WriteLine(number1 + " - " + number2 + " = " + div);
+            Console.WriteLine(number1 + " * " + number2 + " = " + mult);
+            Console.WriteLine(number1 + " / " + number2 + " = " + sub);
+            Console.WriteLine(number1 + " mod " + number2 + " = " + mod);
+        }
+
+        public static void Ex13()
+        {
+            Console.WriteLine("Enter the number");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 10; i++)
+            {
+                int multi = number * i;
+                Console.WriteLine(number + " * " + i + " = " + multi);
+            }
+        }
+
+        public static void Ex14()
+        {
+            Console.WriteLine("Input the first number");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the second number");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the third number");
+            int number3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the fourth number");
+            int number4 = int.Parse(Console.ReadLine());
+
+            int average = (number1 + number2 + number3 + number4) / 4;
+            Console.WriteLine("The average of "+ number1 + ", " + number2  +", " + number3  +", " + number4  +", is:"  + average);
+            
+        }
+
+        public static void Ex15()
+        {
+            
+            Console.WriteLine("Input a string :");
+            string myString = Console.ReadLine();
+
+            char firstChar = myString[0];
+
+            string newString = firstChar + myString + firstChar;
+            Console.WriteLine("The new string is: " + newString);
+
+        }
+
+        public static void Ex16()
+        {
+            int sum = 0;
+            int number = 2;
+            int count = 0;
+           
+            
+               while (count <= 500)
+            {
+                count = 0;
+                for (int j = 2; j <= number / 2; j++)
+                {
+                    if (number % j == 0)
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 0)
+                {
+                        sum = sum + number;
+                        count++;  
+                }
+                number++;
+            }
+               
+               
+            Console.WriteLine("Sum of the first 500 prime numbers: "+ sum);
+        }
+
+    }
+}
---
+++ week02/Week02/Arrays/Program.cs
@@ -0,0 +1,423 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the exercise number you wish to run");
+            int Ex = int.Parse(Console.ReadLine());
+            SelectExercise(Ex);   
+        }
+
+        //1. Find the index of an element in an array.
+
+        public static void Ex01()
+        {
+            
+            int indexfound = -1;
+            List<int> array1 = new List<int> { 12, 7, 67, 69, 5 };
+            string arrayVal = "";
+
+            foreach(int x in array1)
+            {
+                arrayVal += x + ", ";
+            }
+            
+            Console.WriteLine("Enter the element's value you wish to find it's index from the following array: " + arrayVal);
+            int value = int.Parse(Console.ReadLine());
+
+            while (!array1.Contains(value))
+            {
+                Console.WriteLine("Enter only numbers that exist in the above array");
+                value = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < array1.Count; i++)
+            {
+                if (array1[i] == value)
+                {
+                    indexfound = i;
+                    break;
+                }
+            }
+            Console.WriteLine($"Indexfound={indexfound}");
+        }
+
+        //2. Insert an element into an array at a specified position
+
+        public static void Ex02()
+        {
+
+
+            int[] array = new int[] { 1,2,3,4,5};
+            Console.WriteLine("Specify the position you wish to add the element in the array between 0 and 4");
+            int pos = int.Parse(Console.ReadLine());
+
+            while (pos>4)
+            {
+                Console.WriteLine("Enter a value between 0 and 4");
+                pos = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter the value you want to add");
+            int val = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= array.Length-1; i++)
+            {
+                if (i == pos)
+                {
+                    array[i] = val;
+                    break;
+                }
+            }
+            Console.WriteLine("Here is the array: ");
+            foreach (int value in array)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        //For all numbers from 1 to 1000, print the following:
+        //- If number is divisible by 3, print Fizz
+        //- If number is divisible by 5, print Buzz
+        //- If number is divisible by 3 and 5, print FizzBuzz
+        //- Otherwise, print the number
+
+        public static void Ex03()
+        {
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }else if(i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 3 != 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        //02. Write a program to count the frequency of each element in an array e.g. [1, 4, 5, 2, 1, 4, 3, 1, 2]
+        //should output: 1 - 3 times 4 - 2 times 5 - one time 2 - 2 times 3 - one time
+
+        public static void Ex04()
+        {
+            
+            int[] array = new int[10] { 1, 3, 6, 4, 3, 2, 1, 3, 3, 1 };
+
+            bool[] checkComparedNumber = new bool[array.Length];
+
+            for (int i= 0; i < array.Length;i++)
+            {
+
+                if (checkComparedNumber[i] == true)
+                {
+                    continue;
+                }
+                int count = 1;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                        checkComparedNumber[j] = true;
+                    }
+                    
+                }
+                Console.WriteLine(array[i] + " - " + count+" times");
+            }
+        }
+
+        //03. Read an array from the console and separate odd and even values into 2 arrays.
+        //E.g.Number of elements in array: 3
+        //- element 1: 4
+        //- element 2: 3
+        //- element 3: 2
+        //Result: Odd array: [3] Even array: [4, 2]
+
+        public static void Ex05()
+        {
+            Console.WriteLine("Enter the number of elements you want for your array, no more than 10");
+            int arrLength = int.Parse(Console.ReadLine());
+            while (arrLength > 10)
+            {
+                Console.WriteLine("Enter a value < 10");
+                arrLength = int.Parse(Console.ReadLine());
+            }
+            List<int> array = new List<int>(arrLength);
+            var arrayOdd = new List<int>();
+            var arrayEven = new List<int>();
+            Console.WriteLine("Add " + arrLength+" values to array");
+            for (int i = 0; i < arrLength; i++)
+            {
+                array.Add(int.Parse(Console.ReadLine()));
+
+            }
+
+            foreach (int value in array)
+            {
+                if (value % 2 == 0)
+                {
+                    arrayEven.Add(value);
+                }
+                else
+                {
+                    arrayOdd.Add(value);
+                }
+            }
+            Console.WriteLine("Even array:");
+            
+            foreach(int value in arrayEven)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine("Odd array: ");
+
+            foreach (int value in arrayOdd)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+
+        //04. Homework 4
+
+        //1. Find common elements between two arrays of integers.
+
+
+
+        public static void Ex06()
+        {
+
+            Console.WriteLine("Enter the number of elements you want for your first array, no more than 10");
+            int arr1Length = int.Parse(Console.ReadLine());
+            while (arr1Length > 10)
+            {
+                Console.WriteLine("Enter a value < 10");
+                arr1Length = int.Parse(Console.ReadLine());
+            }
+            List<int> array1 = new List<int>(arr1Length);
+            Console.WriteLine("Add " + arr1Length + " values to the first array");
+            for (int i = 0; i < arr1Length; i++)
+            {
+                array1.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine("Enter the number of elements you want for your second array, no more than 10");
+            int arr2Length = int.Parse(Console.ReadLine());
+            while (arr2Length > 10)
+            {
+                Console.WriteLine("Enter a value < 10");
+                arr2Length = int.Parse(Console.ReadLine());
+            }
+            List<int> array2 = new List<int>(arr2Length);
+            Console.WriteLine("Add " + arr2Length + " values to the second array");
+            for (int i = 0; i < arr2Length; i++)
+            {
+                array2.Add(int.Parse(Console.ReadLine()));
+            }
+
+
+            List<int> common = new List<int>();
+            
+            for (int i = 0; i < arr1Length; i++)
+            {
+                for (int j = 0; j < arr2Length; j++)
+                {
+                    if (array1[i] == array2[j])
+                    {
+                        common.Add(array1[i]);
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("The common elements are ");
+            foreach (int value in common)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        //2. Implement an algorithm to determine if a string has all unique characters.
+
+        public static void Ex07()
+        {
+            Console.WriteLine("Enter your string");
+            string myString = Console.ReadLine();
+            bool unique = true;
+
+            for (int i = 0; i < myString.Length; i++)
+            {
+                for (int j = i + 1; j < myString.Length; j++)
+                {
+                    if (myString[i] == myString[j])
+                    {
+                        Console.WriteLine("Your string is not unique");
+                        unique = false;
+                        break;
+                    }
+                }
+            }
+            if (unique==true)
+            {
+                Console.WriteLine("Your string is unique");
+            }
+
+        }
+
+       // 3. Write code to remove duplicates from an unsorted linked list.
+
+
+
+        public static void Ex08()
+        {
+            Console.WriteLine("Enter the number of elements you want for your array, no more than 10");
+            int arrLength = int.Parse(Console.ReadLine());
+            while (arrLength > 10)
+            {
+                Console.WriteLine("Enter a value < 10");
+                arrLength = int.Parse(Console.ReadLine());
+            }
+            List<int> array = new List<int>(arrLength);
+            Console.WriteLine("Add " + arrLength + " values to the first array");
+
+            for(int i = 0; i < arrLength; i++)
+            {
+                array.Add(int.Parse(Console.ReadLine()));
+            }
+
+            var indexArray = new List<int>();
+
+            for (int i = 0; i <= array.Count-1; i++)
+            {
+                for (int j = i + 1; j <= array.Count-1; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        indexArray.Add(i);
+                    }
+                }
+            }
+
+            for (int i = 0; i <= indexArray.Count-1; i++)
+            {
+                array.RemoveAt(indexArray[i]);
+                array.Insert(indexArray[i], 0);
+            }
+            while (array.Contains(0))
+            {
+                array.Remove(0);
+            }
+            
+            Console.WriteLine("Your array without duplicates is ");
+            foreach (int value in array)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        public static void Ex09()
+        {
+            Console.WriteLine("Enter your number");
+            int number = int.Parse(Console.ReadLine());
+
+            bool prime = true;
+
+            for(int i = 2; i <= number / 2; i++)
+                {
+                if (number % i == 0)
+                {
+                    prime = false;
+                }
+            }
+
+            if (prime)
+            {
+                Console.WriteLine("The number is prime");
+            }
+            else
+            {
+                Console.WriteLine("The number is not prime");
+            }
+        }
+        
+
+
+        //Check if a word is a palindrome
+
+
+        public static void Ex10()
+        {
+            Console.WriteLine("Enter your string");
+            string myString = Console.ReadLine();
+            string reverseString = "";
+
+            for (int i = myString.Length - 1; i >= 0; i--)
+            {
+                reverseString = reverseString + myString[i];
+            }
+
+            if (myString == reverseString)
+            {
+                Console.WriteLine("The word is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("The word is not palindrome");
+            }
+
+        }
+
+        public static void SelectExercise(int val)
+        {
+            switch (val)
+            {
+                case 1:
+                    Ex01();
+                    break;
+                case 2:
+                    Ex02();
+                    break;
+                case 3:
+                    Ex03();
+                    break;
+                case 4:
+                    Ex04();
+                    break;
+                case 5:
+                    Ex05();
+                    break;
+                case 6:
+                    Ex06();
+                    break;
+                case 7:
+                    Ex07();
+                    break;
+                case 8:
+                    Ex08();
+                    break;
+                case 9:
+                    Ex09();
+                    break;
+                case 10:
+                    Ex10();
+                    break;
+                  
+
+            }
+        }
+    }
+}
